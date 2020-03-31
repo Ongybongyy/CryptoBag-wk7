ed8c3b911cb42bc36aef91c70152e7a46e1aa82f
Ching Han Ong <chinghan@Chings-MacBook-Pro.local>
2020-03-31 13:42:43 +1100

    Initial commit

---
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/MainActivity.java
@@ -0,0 +1,38 @@
+package au.edu.unsw.infs3634.cryptobag;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import au.edu.unsw.infs3634.cryptobag.Entities.CoinLoreResponse;
+import au.edu.unsw.infs3634.cryptobag.Entities.Coin;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.detail_container) != null) {
+            mTwoPane = true;
+        }
+
+        RecyclerView mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        Gson gson = new Gson();
+        CoinLoreResponse response = gson.fromJson(CoinLoreResponse.json, CoinLoreResponse.class);
+        List<Coin> coins = response.getData();
+
+        RecyclerView.Adapter mAdapter = new CoinAdapter(this, coins, mTwoPane);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
